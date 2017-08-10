19da689e5700499f138a21208f5936e81ca2dfdc
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-08-10 16:48:28 +0100

    Playing with posts to server

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import $ from 'jquery';
 
-const consoleOutput = () => {
-  console.clear();
-  console.log('Hi!');
+// const consoleOutput = () => {
+//   console.clear();
+//   console.log('Hi!');
+// }
+
+const testfuncion = () => {
+  console.log('Let\'s go!');
+  $.ajax({
+    url: "http://192.168.1.11/Timesheet/AddTimesheet",
+    data: {
+      TimeSheetID: "0",
+      StaffID: "18",
+      WeekStartDate: "14/08/2017", // Exact format they need: "19/06/2017"
+      ProjectID: "5396 - Wheatpieces Phase 1A", // "5396 - Wheatpieces Phase 1A"
+      Activity: "Engineering",
+      DEPT: "C",
+      MON: "2.25",
+      TUE: "0",
+      WED: "0",
+      THUR: "0",
+      FRI: "0",
+      WKD: "0",
+      ApproveTimesheet: false,
+      ApproveOT: false
+    },
+    type: "POST",
+    success: function() {
+      console.log('SUCCESSFULL!');
+    },
+    error: function() {
+      console.log('ERROR!');
+    }
+  });    
 }
 
 class Test extends React.Component {
@@ -24,6 +55,7 @@ class Test extends React.Component {
       <div>
         <input type="text" name1="title" placeholder='This is a placehorlder string' value={this.state.title} onChange={this.handleChange.bind(this)} />
         <input type="text" name2="title" placeholder='This is a placehorlder string' value={this.state.title} onChange={this.handleChange.bind(this)} />
+        <button onClick={testfuncion}>HHHHHHHHHHHHHHH</button>
       </div>
     )
   }
