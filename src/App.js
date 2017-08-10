57ec08f2b6f7ad556f955f44c978fcfed697f7b4
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-08-10 07:10:35 +0100

    Playing around with forms

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     // Pass props to parent class
     super (props);
     this.state = {
-      testMode: true,
+      testMode: false,
       // selectedMenu: 'Overview'
       // selectedMenu: 'Dashboard'
       selectedMenu: 'Time sheets'
